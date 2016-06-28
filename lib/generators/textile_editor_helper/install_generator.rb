abeef0e98f5b21ca7d955b7b9e4eb8975a03caa3
Daniela van der Heijden <info@wwwebsites.com>
2016-06-29 00:06:25 +0200

    first commit daniela

--- lib/generators/textile_editor_helper/install_generator.rb
+++ lib/generators/textile_editor_helper/install_generator.rb
@@ -6,10 +6,17 @@ module TextileEditorHelper
       def copy_assets
         js_destination = 'app/assets/javascripts'
         css_destination = 'app/assets/stylesheets'
+        layout_destination = 'app/views/layouts'
+        initializers_destination = 'config/initializers'
+        copy_file 'assets/javascripts/backend.js', "#{js_destination}/backend.js"
         copy_file 'assets/javascripts/textile-editor.js', "#{js_destination}/textile-editor.js"
         copy_file 'assets/javascripts/textile-editor-config.js', "#{js_destination}/textile-editor-config.js"
-        copy_file 'assets/stylesheets/textile-editor.css', "#{css_destination}/textile-editor.css"
-        directory 'assets/images/textile-editor', 'app/assets/images/textile-editor'
+        copy_file 'assets/stylesheets/backend.css', "#{css_destination}/backend.css"
+        copy_file 'assets/stylesheets/backend.scss', "#{css_destination}/backend/backend.scss"
+        copy_file 'assets/stylesheets/textile-editor.scss', "#{css_destination}/backend/textile-editor.scss"
+        copy_file 'assets/views/backend.html.erb', "#{layout_destination}/backend.html.erb"
+        copy_file 'assets/config/backend.rb', "#{initializers_destination}/backend.rb"
+        #directory 'assets/images/textile-editor', 'app/assets/images/textile-editor'
         readme 'README' if behavior == :invoke
       end
     end
