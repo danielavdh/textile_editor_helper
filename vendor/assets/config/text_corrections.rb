62b12b9e6eb138c0d96883a84d467632a1d40cce
Daniela van der Heijden <info@wwwebsites.com>
2016-10-15 23:36:52 +0100

    new RedCloth to text_corrections

--- vendor/assets/config/text_corrections.rb
+++ vendor/assets/config/text_corrections.rb
@@ -2,28 +2,26 @@ module TextCorrections
   
   def self.images_and_links(item_text)
     text = item_text
-    content = textilize(text).gsub(/href="http:\/\//, "target=\"_blank\" href=\"http://")
-    #if content.split("<img src=").length > 0
+    #content = textilize(text).gsub(/href="http:\/\//, "target=\"_blank\" href=\"http://")
+    content = RedCloth.new(text).to_html.gsub(/href="https:\/\//, "target=\"_blank\" href=\"https://").gsub(/href="http:\/\//, "target=\"_blank\" href=\"http://")
     content = content.gsub(/\<img src=\"(\d+)\"/) do |mstr|
       begin
         doc = Document.find($1)
-        #unless doc.size == "large"
-        #  "<img src=\"#{doc.uploadedPhoto.url(doc.size)}\" class=\"img_float_#{doc.float}\""
-        #else
-        #  "<img src=\"#{doc.uploadedPhoto.url(doc.size)}\" class=\"large_image\""
-        #end
-          "<img src=\"#{doc.uploadedPhoto.url(doc.size)}\""
+        "<img src=\"#{doc.uploadedPhoto.url(doc.size)}\""
       rescue ActiveRecord::RecordNotFound
         #the error is now handled in article validations
         #rescue still needed preview part when rendering page with error
       end
     end
-    content.html_safe
-    #end
+    #content.html_safe
+    content
   end
   
+#  def self.get_external_links(text)
+#    content = textilize(text).gsub(/href="http:\/\//, "target=\"_blank\" href=\"http://")
+#  end
   def self.get_external_links(text)
-    content = textilize(text).gsub(/href="http:\/\//, "target=\"_blank\" href=\"http://")
+    content = RedCloth.new(text).to_html.gsub(/href="https:\/\//, "target=\"_blank\" href=\"https://").gsub(/href="http:\/\//, "target=\"_blank\" href=\"http://")
   end
 
 end
